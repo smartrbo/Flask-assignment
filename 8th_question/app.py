dba3374138532790193db841490d926f68b56593
kunalsmartrbo <kunalappa523@gmail.com>
2023-12-18 19:44:31 +0530

    last commit

---
+++ 8th_question/app.py
@@ -0,0 +1,51 @@
+# 8. Implement user authentication and registration in a Flask app using Flask-Login.
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key' 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin):
+    def __init__(self, user_id):
+        self.id = user_id
+
+users = {
+    'user1': {'password': 'password1'},
+    'user2': {'password': 'password2'},
+}
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username]['password'] == password:
+            user = User(username)
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Login failed. Check your username and password.', 'danger')
+    return render_template('index.html')
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return f'Hello, {current_user.id}! This is your dashboard.'
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=8000)
